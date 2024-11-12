949260ed94f2f8365e61aa708d5f99adfd2761fb
Raphael <100046113+RaphaelBarcelos@users.noreply.github.com>
2024-11-12 10:13:46 -0300

    home

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/CarouselAdapter.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHolder> {
+
+    private final int[] images;
+    private final String[] texts;
+
+    public CarouselAdapter(int[] images, String[] texts) {
+        this.images = images;
+        this.texts = texts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(images[position]);
+        holder.textView.setText(texts[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView textView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+
+        int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+        String[] texts = {"Texto 1", "Texto 2", "Texto 3"};     
+
+        CarouselAdapter adapter = new CarouselAdapter(images, texts);
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+}
